15861c8d60b5462a06c1c4590e9e6abbb842db05
vinicius <vinicius.alexandre.gomes10@aluno.ifce.edu.br>
2022-05-17 01:41:35 -0300

    SALVE ME DEUS

--- index.js
+++ index.js
@@ -19,14 +19,30 @@
  */
 
  app.set("views", path.join(__dirname, "views"));
- app.set("view engine", "pug");
+ app.set("view engine", "ejs");
 
 /**
  * Routes Definitions
  */
 
   app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+    res.render("home", { title: "Home" });
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login", { title: "Login" });
+  });
+
+  app.get("/signin", (req, res) => {
+    res.render("signIn", { title: "SignIn" });
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about", { title: "About" });
+  });
+
+  app.get("/mainNotLogged", (req, res) => {
+    res.render("mainNotLogged", { title: "Main" });
   });
 
 /**
